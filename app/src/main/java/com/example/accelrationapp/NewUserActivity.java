66ea4fe4eabb8d814b3934cbf5b25fbd1249dc1c
Semir Hodzic <semir@smartson.se>
2020-11-21 12:32:58 +0100

    Refactor name of newUserActivity

--- app/src/main/java/com/example/accelrationapp/NewUserActivity.java
+++ app/src/main/java/com/example/accelrationapp/NewUserActivity.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Activity_newuser extends AppCompatActivity {
+public class NewUserActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
