d65d45472b50ea446e43db967237a1cdfaa3c92a
AwJindo <73054961+AwJindo@users.noreply.github.com>
2020-11-21 14:12:44 +0100

    Done All the activities, all layouts not done yet but exists

---
+++ app/src/main/java/com/example/accelrationapp/AcceptTestValuesActivity.java
@@ -0,0 +1,14 @@
+package com.example.accelrationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AcceptTestValuesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accept_test_values);
+    }
+}
---
+++ app/src/main/java/com/example/accelrationapp/ShowValuesFromHistoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.accelrationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowValuesFromHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_values_from_history);
+    }
+}
