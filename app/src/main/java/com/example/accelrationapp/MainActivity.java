70cacb121a447c98fb3d0384beec363ce255b1d5
AwJindo <73054961+AwJindo@users.noreply.github.com>
2020-11-22 13:09:01 +0100

    Changed layout on activity_user_info.xml, Send variable through NewUserActivity to UserInfoActivity. Social security number to large for int need to change that. Need to change activity_user_info.xml layout again.

--- app/src/main/java/com/example/accelrationapp/MainActivity.java
+++ app/src/main/java/com/example/accelrationapp/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.accelrationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button newuser;
+    private Button existinguser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        newuser = (Button) findViewById(R.id.newuser);
+        newuser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openNewUserActivity();
+            }
+        });
+        existinguser = (Button) findViewById(R.id.existinguser);
+        existinguser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openExistingUserActivity();
+            }
+        });
+    }
+
+    public void openNewUserActivity(){
+        Intent intent = new Intent(this, NewUserActivity.class);
+        startActivity(intent);
+    }
+    public void openExistingUserActivity(){
+        Intent intent = new Intent(this, ExistingUserActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/accelrationapp/UserInfoActivity.java
+++ app/src/main/java/com/example/accelrationapp/UserInfoActivity.java
@@ -2,7 +2,11 @@ package com.example.accelrationapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class UserInfoActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class UserInfoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+
+        Intent intent = getIntent();
+        String text = intent.getStringExtra(NewUserActivity.EXTRA_TEXT);
+        int number1 = intent.getIntExtra(NewUserActivity.EXTRA_NUMBER1,0);
+        int number2 = intent.getIntExtra(NewUserActivity.EXTRA_NUMBER2,0);
+        int number3 = intent.getIntExtra(NewUserActivity.EXTRA_NUMBER3,0);
+        int number4 = intent.getIntExtra(NewUserActivity.EXTRA_NUMBER4,0);
+
+        TextView text_view_name = findViewById(R.id.text_view_name);
+        TextView text_view_socialnumber = findViewById(R.id.text_view_socialnumber);
+        TextView text_view_age = findViewById(R.id.text_view_age);
+        TextView text_view_weight = findViewById(R.id.text_view_weight);
+        TextView text_view_length = findViewById(R.id.text_view_length);
+
+        text_view_name.setText(text);
+        text_view_socialnumber.setText("" + number1);
+        text_view_age.setText(""+ number2);
+        text_view_weight.setText(""+number3);
+        text_view_length.setText(""+number4);
     }
 }
